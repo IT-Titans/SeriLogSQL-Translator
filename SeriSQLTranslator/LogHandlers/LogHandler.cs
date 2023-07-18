2e264a0cd55b5bced1af7903225dcd54172f25f5
Titan-Dominic <139745366+Titan-Dominic@users.noreply.github.com>
2023-07-18 12:52:56 +0200

    First Commit for Class Library

---
+++ SeriSQLTranslator/LogHandlers/LogHandler.cs
@@ -0,0 +1,22 @@
+﻿using Serilog.Events;
+using SeriSQLTranslator.Services;
+
+namespace SeriSQLTranslator.LogHandlers;
+
+public static class LogHandler 
+{
+    public static void HandleLogEvent(LogEvent logEvent)
+    {
+        var parserService = new ParserService();
+        
+        string logMessage = logEvent.RenderMessage().Replace("\"", String.Empty);
+        string parsedLogMessage = parserService.ParseLogStringToSqlCommand(logMessage);
+        
+        var consoleFormatter = new ConsoleFormatterService();
+
+        Console.Out.Write(logEvent.Timestamp + " | ");
+        consoleFormatter.FormatLogLevelOutput(logEvent);
+        Console.Out.Write(parsedLogMessage);
+        Console.Out.WriteLine();
+    }
+}
---
+++ SeriSQLTranslator/Models/SqlCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace SeriSQLTranslator.Models;
+
+/// <summary>
+/// Model which represents an sql command. 
+/// </summary>
+public class SqlCommand
+{
+    /// <summary>
+    /// The command string of an sql command containing sql keywords and parameters.
+    /// </summary>
+    public string Command { get; set; } = string.Empty;
+}
